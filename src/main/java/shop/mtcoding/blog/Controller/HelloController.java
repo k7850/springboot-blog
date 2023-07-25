c2248095766a1f2233b984ec93e04d2ce1b01ab0
k7850 <k79357850@gmail.com>
2023-07-25 16:57:32 +0900

    mustache연습

--- src/main/java/shop/mtcoding/blog/Controller/HelloController.java
+++ src/main/java/shop/mtcoding/blog/Controller/HelloController.java
@@ -1,15 +1,56 @@
 package shop.mtcoding.blog.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Controller
 public class HelloController {
-    
-    @GetMapping(value="test")
-    public String test() {
+
+    @GetMapping("/test")
+    public String test(Model model) {
+        model.addAttribute("username", "ssar");
         return "test";
     }
-    
+
+    @GetMapping("/array")
+    public String test2(Model model) {
+        List<String> list = new ArrayList<>();
+        list.add("바나나");
+        list.add("딸기");
+        list.add("수박");
+        model.addAttribute("list", list);
+        return "array";
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    class User {
+        private int id;
+        private String username;
+    }
+
+    @GetMapping("/users")
+    public String users(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User(1, "ssar"));
+        list.add(new User(2, "cos"));
+        model.addAttribute("list", list);
+        boolean yougood = false;
+        model.addAttribute("yougood", true);
+        return "users";
+    }
+
 }
