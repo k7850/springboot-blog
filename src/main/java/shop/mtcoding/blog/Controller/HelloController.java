fa1ddbd4d399c8760ff10e15cb5f3887841df6e7
k7850 <k79357850@gmail.com>
2023-07-25 15:53:22 +0900

    처음설정

---
+++ src/main/java/shop/mtcoding/blog/Controller/HelloController.java
@@ -0,0 +1,15 @@
+package shop.mtcoding.blog.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HelloController {
+    
+    @GetMapping(value="test")
+    public String test() {
+        return "test";
+    }
+    
+}
